df63a1eafc11129a777f11981aa52281708f07ca
Giuseppe Calderaro <giuseppecalderaro@gmail.com>
2011-11-03 21:29:33 +0000

    Added SystemC template project

---
+++ SystemC/SystemC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SystemC
+//
+//  Created by Giuseppe Calderaro on 03/11/2011.
+//  Copyright (c) 2011 Arm. All rights reserved.
+//
+
+#include "systemc.h"
+
+int sc_main (int argc, char * argv[]) {
+    
+    // insert code here...
+    std::cout << "Hello, SystemC!\n";
+    sc_start();
+    return 0;
+}
+
